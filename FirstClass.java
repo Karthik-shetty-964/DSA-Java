6585df2b3de55e136dfd77bd04407b8d1c486d46
Karthik shetty <karthiksshetty111@gmail.com>
2022-05-01 11:11:47 +0530

    patter prblems

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,14 @@
+import java.util.*;
 class FirstClass{
     public static void main(String args[]){
-        System.out.println("wtf bro");
-    }
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                System.out.println("not a prime");
+                System.exit(0);
+            }
+        }
+        System.out.println("Is a prime");
+    }  
 }
