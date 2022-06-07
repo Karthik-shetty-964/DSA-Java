3622784ce0b72a7ab6762dce94d4347fb84477b1
Karthik shetty <karthiksshetty111@gmail.com>
2022-06-07 22:35:29 +0530

    backtracking

--- Backtracking/Sudoku.java
+++ Backtracking/Sudoku.java
@@ -14,14 +14,66 @@ Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4",
 */
 import java.util.*;
 public class Sudoku{
-    public static boolean helper(char[][] board,int row,int col){
-        
+    public static boolean isSafe(char[][] board,int row,int col,int val){
+        //horizotal and vertical 
+        for(int i=0;i<board.length;i++){
+            if(board[row][i]==(char)(val+'0')){
+                return false;
+            }
+            if(board[i][col]==(char)(val+'0')){
+                return false;
+            }
+        }
+        //grid
+        int rs=(row/3)*3;
+        int cs=(col/3)*3;
+        for(int r=rs;r<rs+3;r++){
+            for(int c=cs;c<cs+3;c++){
+                if(board[r][c]==(char)(val+'0')){
+                    return false;                    
+                }
+            }
+        }
+       return true; 
     }
+    public static boolean helper(char [][] board,int row,int col){
+        if(row==board.length){
+            return true;
+        }
+        int nrow=0;
+        int ncol=0;
+        if(col!=board.length-1){
+             nrow=row;
+             ncol=col+1;
+        }else{
+             nrow=row+1;
+             ncol=0;
+        }
+        if(board[row][col]!='.'){
+            if(helper(board,nrow,ncol)){
+                return true;
+            }
+        }else{
+             for(int i=1;i<=9;i++){
+                if(isSafe(board,row,col,i)){
+                    board[row][col]=(char)(i+'0');
+                    if(helper(board,nrow,ncol)){
+                        return true;
+                    }else{
+                        board[row][col]='.';
+                    }
+                   
+                }
+            }
+        }
+        return false;
+    }
+   
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         char [][] board={{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
         
-         helper(board,0,0);
+        helper(board,0,0);
 
         //print Output
         for(int i=0;i<board.length;i++){
